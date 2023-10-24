3ef608d3eb0843db1368e42366029653bb80c761
Sparsegull2 <142937470+Sparsegull2@users.noreply.github.com>
2023-10-24 17:27:21 -0400

    Add files via upload

---
+++ Lab10A.cs
@@ -0,0 +1,55 @@
+﻿/* 
+Class: CSE 1321L 
+Section: #03 
+Term: Fall 2023
+Instructor: John Blake 
+Name: Christopher Morrison
+Lab#: Lab10A
+*/
+
+using System;
+
+class Lab10A
+{
+    public static int maximum (int x,int y)
+    {
+        if (x > y)
+        {
+            return x;
+        }
+        else
+        {
+            return y;
+        }
+    }
+    public static int minimum(int x, int y) 
+    {
+        if (x < y)
+        {
+            return x;
+        }
+        else 
+        { 
+            return y;
+        }
+    }
+    public static float avaerage(int x,int y)
+    {
+        float avg = ((float)x + (float)y) / 2;
+        return avg;
+    }
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter number 1: ");
+        int num1= int.Parse(Console.ReadLine());
+        Console.Write("Enter number 2: ");
+        int num2= int.Parse(Console.ReadLine());
+        int min = minimum(num2, num1);
+        Console.WriteLine("Min is " + min);
+        int max = maximum(num1, num2);
+        Console.WriteLine("Max is "+max);
+        float avg= avaerage(num1, num2);
+        Console.WriteLine("Average is "+avg);
+        
+    }
+}
---
+++ Lab10B.cs
@@ -0,0 +1,57 @@
+﻿/* 
+Class: CSE 1321L 
+Section: #03 
+Term: Fall 2023
+Instructor: John Blake 
+Name: Christopher Morrison
+Lab#: Lab10B
+*/
+
+using System;
+
+class Lab10B
+{
+    public static bool isValid(double width, double height) 
+    {
+        if (width + height > 30)
+        {
+            return true;
+        }
+        else return false;
+    }
+    public static double area(double width, double height) 
+    {
+        return width * height;
+    }
+    public static double perimeter(double width, double height) 
+    { 
+        return (width + height)*2;
+    }
+    public static void Main(string[] args)
+    {
+        bool cont = true;
+        while (cont)
+        {
+            Console.Write("Enter width: ");
+            double width = double.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            double height = double.Parse(Console.ReadLine());
+            if(isValid(width, height) )
+            {
+                double a= area(width, height);
+                double p= perimeter(width, height);
+                Console.WriteLine("This is a valid rectangle");
+                Console.WriteLine("The area is: "+a);
+                Console.WriteLine("The perimeter is: "+p);
+            }
+            else
+            {
+                Console.WriteLine("This is an invalid rectangle");
+            }
+            Console.Write("\nDo you want to enter another width and height (Y/N)? : ");
+            char another= char.Parse(Console.ReadLine());
+            if(another=='N') cont= false;
+        }
+        Console.WriteLine("Program Ends");
+    }
+}
